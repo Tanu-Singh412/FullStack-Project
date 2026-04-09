af46c0bb7337f952add5d344302f62a933715097
Tanu-Singh412 <stanu4singh@gmail.com>
2026-04-09 17:22:09 +0530

    updated backend and fixed MongoDB config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/ArchitectPlatform");
+  await mongoose.connect("mongodb+srv://stanu4singh:tanu123456@cluster0.fx8em.mongodb.net/ArchitectPlatform?appName=Cluster0");
 
   console.log("MongoDB Connected");
 };
