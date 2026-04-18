e8826c9a5872182a2fa7ca9545d40739280aef42
Tanu-Singh412 <stanu4singh@gmail.com>
2026-04-18 11:24:32 +0530

    mm

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,18 +1,22 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "../uploads");
+
+// ensure folder exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadPath);
   },
 
   filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + "-" + file.originalname
-    );
+    cb(null, Date.now() + "-" + file.originalname);
   },
-
 });
 
 module.exports = multer({ storage });
