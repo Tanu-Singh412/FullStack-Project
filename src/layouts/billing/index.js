bcdc0715de9f869eca7f18fdf6e9c8bef42650e5
Tanu-Singh412 <stanu4singh@gmail.com>
2026-04-23 11:45:30 +0530

    MM

--- src/layouts/billing/index.js
+++ src/layouts/billing/index.js
@@ -1,4 +1,5 @@
-import React, { useRef, useState, useEffect, useNavigate } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
